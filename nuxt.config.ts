0542a03079e420f2622705b5e4d5c9aa1bcc83bd
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2023-10-14 21:36:11 +0900

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
-<<<<<<< HEAD
-
-=======
->>>>>>> dd14b984949991ebe58882647e1d16c6fec462c5
+  devtools: { enabled: true },
+});
