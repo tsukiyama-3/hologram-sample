2bfa60cb9313fd4cf0d3455773f4cec08209e9d4
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2023-10-14 22:31:56 +0900

    add tailwind

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-const absolute = ref(false);
 const alpha = ref<number | null>(null);
 const beta = ref<number | null>(null);
 const gamma = ref<number | null>(null);
 
 const handleOrientation = (e: DeviceOrientationEvent) => {
-  absolute.value = e.absolute;
   alpha.value = e.alpha;
   beta.value = e.beta;
   gamma.value = e.gamma;
 };
 
-const gammaPercent = computed(() => {
+const gammaDeg = computed(() => {
   if (!gamma.value) return "0deg";
   return `${(gamma.value + 90) * 2}deg`;
 });
@@ -38,23 +36,27 @@ const requestDeviceMotionPermission = () => {
 </script>
 
 <template>
-  <div>
-    <div class="container">
-      <div class="box">
+  <div class="space-y-4">
+    <div class="flex h-[50vh] justify-center items-center">
+      <div
+        class="w-[300px] h-[300px] flex items-center justify-center bg-[length:30px_30px] box"
+      >
         <img
           src="https://res.cloudinary.com/ddyzss9j2/image/upload/v1697272733/icon/Rectangle_1_iqgv23.png"
           width="300"
         />
       </div>
     </div>
-    <div class="device-motion-info">
-      <p>absolute: {{ absolute }}</p>
-      <p>alpha: {{ alpha }}</p>
-      <p>beta: {{ beta }}</p>
-      <p>gamma: {{ gamma }}</p>
+    <div class="text-center">
+      <p>alpha: {{ Math.round(alpha!) ?? "-" }}</p>
+      <p>beta: {{ Math.round(beta!) ?? "-" }}</p>
+      <p>gamma: {{ Math.round(gamma!) ?? "-" }}</p>
     </div>
-    <div class="btn-container">
-      <button @click="requestDeviceMotionPermission" class="permission-btn">
+    <div class="flex justify-center">
+      <button
+        @click="requestDeviceMotionPermission"
+        class="text-base bg-emerald-400 rounded-md px-6 py-3 text-white cursor-pointer"
+      >
         傾きを検知する
       </button>
     </div>
@@ -62,47 +64,15 @@ const requestDeviceMotionPermission = () => {
 </template>
 
 <style scoped>
-.container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
 .box {
-  width: 300px;
-  height: 300px;
   background-image: repeating-conic-gradient(
-    from v-bind(gammaPercent),
+    from v-bind(gammaDeg),
     hsl(232, 100%, 50%) 0%,
     hsl(177, 100%, 50%) 25%,
     hsl(0, 0%, 100%) 50%,
     hsl(177, 100%, 50%) 75%,
     hsl(232, 100%, 50%) 100%
   );
-  background-size: 30px 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   filter: drop-shadow(4px 4px 4px rgb(0, 0, 0));
-}
-
-.device-motion-info {
-  text-align: center;
-}
-
-.btn-container {
-  margin-top: 2rem;
-  display: flex;
-  justify-content: center;
-}
-
-.permission-btn {
-  font-size: 1rem;
-  cursor: pointer;
-  background-color: hsl(177, 100%, 50%);
-  border-radius: 6px;
-  border: none;
-  padding: 0.75rem 1.5rem;
-  filter: drop-shadow(0 1px 0 rgb(0, 0, 0));
 }
 </style>
